b5c33d6def8b36b2ae0d8fb688a769a7785b0caa
shipra <fshipra@seattleu.edu>
2023-02-26 15:27:30 -0800

    Adding the code to display the results in the browser

--- address.js
+++ address.js
@@ -14,7 +14,7 @@ xhr.onreadystatechange = function() {
   
         var columns = row.split(',');
         var address = {
-          ship_address: columns[0].trim(),
+          search_address: columns[0].trim(),
           city: columns[1].trim(),
           state: columns[2].trim(),
           postalcode: columns[3].trim(),
@@ -26,3 +26,34 @@ xhr.onreadystatechange = function() {
   };
   console.log(addresses);
   xhr.send();
+
+  var searchBox = document.getElementById('search_address');
+  var city = document.getElementById('city');
+  var state = document.getElementById('state');
+  var country = document.getElementById('country');
+  var postalcode = document.getElementById('postalcode');
+
+  searchBox.addEventListener('input', function() {
+    var searchTerm = this.value.toLowerCase();
+    var matchingAddresses = addresses.filter(function(address) {
+      return address.state.toLowerCase().includes(searchTerm) || address.city.toLowerCase().includes(searchTerm) || address.country.toLowerCase().includes(searchTerm);
+    });
+    console.log("matching address " + matchingAddresses);
+
+    searchBox.focus();
+    // var html = '';
+    // matchingAddresses.forEach(function(address) {
+    //   html += '<div>' + address.city + ', ' + address.state + ', ' + address.country + ', ' + address.postalcode + '</div>';
+    // });
+    // var results = document.getElementById('search-box-results');
+    // results.innerHTML = html;
+
+    // var searchBoxResults = document.querySelectorAll('#search-box-results div');
+    // searchBoxResults.forEach(function(result) {
+    //   result.addEventListener('click', function() {
+    //     search_address.value = searchBox;
+    //     city.value = address.city;
+    //     state.value = address.state;
+    //   });
+    // });
+  });
